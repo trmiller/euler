5d16184bf2c47c1bf01b807da893b31140995821
trmiller <trmiller@gmail.com>
2013-03-03 22:28:54 -0500

    cleanup

--- special_pythagorean_triplet.py
+++
@@ -1,9 +0,0 @@
-for a in xrange(1,800):
-    for b in xrange(a+1, 700):
-        for c in xrange(b+1, 800):
-            if a*a + b*b == c*c:
-                if a + b + c == 1000:
-                    print "a = ", a, ", b = ", b , ", c = ", c
-                    print a*b*c
-                    break
-
