76b639c7102b71a536a20797ee1791c6a2f3a141
Tim Miller <trmiller@gmail.com>
2013-03-05 20:58:38 -0500

    question 12

---
+++ src/highly_divisible_triangular_number.py
@@ -0,0 +1,31 @@
+import math
+
+def getFactorCount(num):
+    count = 0
+    root = long(math.sqrt(num))
+    for i in xrange(1,root + 1):
+        if num % i == 0:
+            count += 2
+    if root * root == num:
+        count =- 1
+        
+    return count
+
+factorCount = 0
+i = 1
+triangle = 0
+while True:
+    triangle += i
+    factorCount = getFactorCount(triangle)
+    print triangle, " - ", factorCount
+    if factorCount >= 500:
+        print "winner winner chicken dinner"
+        break
+    i += 1
+print "done" 
+
+
+#for i in sorted(triangle_numbers.iterkeys()):
+#    print i
+#    for j in triangle_numbers[i]:
+#        print "\t ", j
