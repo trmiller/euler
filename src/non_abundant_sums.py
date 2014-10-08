9c6d734b659a52432b4437a166e62dbb2e8613c0
trmiller <trmiller@gmail.com>
2014-10-07 23:38:07 -0400

    well i feel dumb on this one - leaving the old code in place to look at later

--- src/non_abundant_sums.py
+++ src/non_abundant_sums.py
@@ -1,7 +1,15 @@
 
-def non_abundant_sums(self):
-    abundant_numbers = get_non_abundant_sums(28124)
-    print abundant_numbers
+def problem_23(self):
+    top_limit = 28123
+    sieve = [False] * top_limit
+    abundant_numbers = [x for x in xrange(12, top_limit) if is_abundant_number(x)]
+    
+    for i in xrange(len(abundant_numbers)): 
+        for j in xrange(i,len(abundant_numbers)):
+            if abundant_numbers[i]+abundant_numbers[j]<top_limit:
+                sieve[abundant_numbers[i]+abundant_numbers[j]]=True 
+
+    print sum([i for i in xrange(len(sieve)) if not(sieve[i])])
 
 def is_abundant_sum(num, divisors, abundant_nums):
     count = 0
@@ -11,37 +19,27 @@ def is_abundant_sum(num, divisors, abundant_nums):
         if i in abundant_nums:
             count = count + 1
         if count == 2:
-            return False
-    return True
+            return True
+    return False
     
-def get_non_abundant_sums(top_limit):
+def get_non_abundant_sums(top_limit, abundant_numbers):
     sum = 0
-    abundant_numbers = []
     for i in xrange(1, top_limit):
-        print i
         divisors = get_proper_divisors(i)
-        #print divisors
-        
-        if is_abundant_number(i, divisors):
-            #print 'abundant number'
-            abundant_numbers.append(i)
-        
-        if is_abundant_sum(i, divisors, abundant_numbers) == True:
+        if is_abundant_sum(i, divisors, abundant_numbers) is False:
+            print '{} is non abundant sum'.format(i)
             sum = sum + i
-        else:
-            print '\tis abundant sum'
-        
-        #print '-------------------'
     
     #print 'all abundant nums'
     #print abundant_numbers
     return sum
 
-def is_abundant_number(num, divisors):
-    
-    if sum(divisors) > num:
-        return True
-    return False
+def is_abundant_number(num):
+    sum=0
+    for i in xrange(1,num/2+1):
+        if num%i==0:
+            sum+=i
+    return sum>num
 
 def get_proper_divisors(num):
     divisors = []
@@ -53,4 +51,4 @@ def get_proper_divisors(num):
     return divisors
     
 if __name__ == '__main__':
-    print non_abundant_sums(None)
+    print problem_23(None)
